2ed92cbf889a252f5d702857b4a98e2f0ba8081e
Kane <concealedcharacterx@gmail.com>
2025-03-11 11:42:11 +0700

    Create experiment-3.R

---
+++ BF4725/experiment-3.R
@@ -0,0 +1,7 @@
+# Cot delta_t o trong file excel Experiment 3 dang nhe ra phai la delta_d nhung ma tui ghi nham hehe.
+
+ggplot(Experiment_3, aes(x = time, y = delta_t)) +
+geom_line() +
+geom_point(shape = 25, size = 3, fill = "#fee0d2") +
+stat_smooth(method = lm, se = FALSE, colour = "#de2d26") +
+annotate("text", x = 62.5, y = 0.78, label = "R^2 == 0.9997061", parse = TRUE)
