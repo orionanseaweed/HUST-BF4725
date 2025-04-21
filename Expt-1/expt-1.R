67890e21b9e0e7c25cc56c62f352d74a6e2c5a15
Kane <concealedcharacterx@gmail.com>
2025-04-22 00:45:04 +0700

    Update expt-1.R

--- Expt-1/expt-1.R
+++ Expt-1/expt-1.R
@@ -1,14 +1,17 @@
-e <- ggplot(experiment_1, aes(x = time)) +
-geom_line(aes(y = t, color = "T"), size = 1) +
-geom_line(aes(y = delta_t, color = "\u0394T"), size = 1, linetype = "dashed") +
-scale_y_continuous(
-  name = "T (°C)",
-  sec.axis = sec_axis(~./1, name = "\u0394T (°C)"),
-  limits = c(0, 60)
-) +
-labs(x = "Thời gian (phút)", color = "Chú thích") +
-theme_bw() +
-theme(panel.border = element_blank(), axis.line = element_line(colour = "black")) +
-theme(legend.position = c(.85, .2)) +
-theme(legend.background = element_rect(fill = "white", colour = "black")) +
-theme(axis.line = element_line(arrow = arrow(length = unit(0.25, "cm"), type="closed")))
+e <- ggplot(expt_1, aes(x = time)) +
+  geom_line(aes(y = t, color = "T"), size = 1) +
+  geom_line(aes(y = delta_t, color = "\u0394T"), size = 1, linetype = "dashed") +
+  scale_y_continuous(
+    name = "T (°C)",
+    sec.axis = sec_axis(~./1, name = "\u0394T (°C)"),
+    limits = c(0, 60)
+  ) +
+  labs(x = "Thời gian (phút)", color = "Chú thích") +
+  theme(
+    panel.border = element_blank(),
+    axis.line = element_line(colour = "black")
+  ) +
+  theme(legend.position = c(.85, .2)) +
+  theme(legend.background = element_rect(fill = "white", colour = "black")) +
+  theme(axis.line = element_line(arrow = arrow(length = unit(0.25, "cm"), type="closed"))) +
+  theme_bw()
